874efb391891512645d96362059013afbf52f00f
leejaejoon <jaejoon0721@ajou.ac.kr>
2024-05-02 16:07:51 +0900

    init: tailwind 세팅, alias 에러

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import "../styles/global.css";
+//import "~/styles/global.css"; 에러
+
 function App() {
-  return <>후비고홈</>;
+  return <div className="bg-red-300">후비고홈</div>;
 }
 
 export default App;
