8c04bea3d6ee08b60a9e45e931dac0845716cefc
Cuzz <rygus9@naver.com>
2024-05-03 00:34:51 +0900

    build: vite + https 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-//import mkcert from "vite-plugin-mkcert";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   https: true,
-  // },
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    // mkcert({
-    //   // SSL 키 등록
-    //   // keyFile: "/localhost-key.pem",
-    //   // certFile: "/localhost.pem",
-    // }),
-  ],
+  plugins: [react(), tsconfigPaths(), basicSsl()],
 });
