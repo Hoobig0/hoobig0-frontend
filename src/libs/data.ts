773560e889aa26bd99aa57b34000a16bfbcd05f8
junly21 <jaejoon0721@ajou.ac.kr>
2024-05-20 03:44:37 +0900

    feat: 지도 불러오기, 마크 출력

---
+++ src/libs/data.ts
@@ -0,0 +1,22 @@
+export const cctv_list = [
+  {
+    cctv_id: 1,
+    cctv_x: "37.2793594",
+    cctv_y: "127.0434841",
+  },
+  {
+    cctv_id: 2,
+    cctv_x: "37.26530",
+    cctv_y: "126.99822",
+  },
+  {
+    cctv_id: 3,
+    cctv_x: "37.2787829",
+    cctv_y: "127.0440551",
+  },
+  {
+    cctv_id: 4,
+    cctv_x: "37.2779634",
+    cctv_y: "127.0453627",
+  },
+];
--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,13 +1,48 @@
+import { useEffect } from "react";
 import { createLazyFileRoute } from "@tanstack/react-router";
-
+import cctvImage from "/cctv.svg";
+import { cctv_list } from "../libs/data";
 export const Route = createLazyFileRoute("/")({
   component: Index,
 });
-
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
 function Index() {
+  useEffect(() => {
+    const container = document.getElementById("map");
+    const options = {
+      center: new window.kakao.maps.LatLng(37.279248, 127.044517),
+      level: 3,
+    };
+
+    const map = new window.kakao.maps.Map(container as HTMLElement, options);
+    const imageSrc = cctvImage;
+    const imageSize = new window.kakao.maps.Size(24, 35);
+    const markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize);
+    cctv_list.map((cctv) => {
+      new window.kakao.maps.Marker({
+        map: map,
+        position: new window.kakao.maps.LatLng(cctv.cctv_x, cctv.cctv_y),
+        image: markerImage,
+      });
+    });
+  }, []);
+
   return (
-    <div className="bg-red-300 p-2">
-      <h3> 후비고홈 </h3>
-    </div>
+    <>
+      <h1>아주대 맛집이지만 CCTV인척</h1>
+      <div
+        id="map"
+        style={{
+          width: "800px",
+          height: "800px",
+          border: "1px solid black",
+        }}></div>
+    </>
   );
 }
+
+export default Index;
